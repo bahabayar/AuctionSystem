a04bfb30ca27c967007508c40d1e133cd6ca4fa2
Baha <bahabayar@hotmail.com>
2021-05-27 11:59:20 +0300

    Creating RabbitMQ Connection Class

--- EventBusRabbitMQ/IRabbitMQPersistentConnection.cs
+++ EventBusRabbitMQ/IRabbitMQPersistentConnection.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RabbitMQ.Client;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,11 @@ using System.Threading.Tasks;
 
 namespace EventBusRabbitMQ
 {
-   public interface IRabbitMQPersistentConnection
+    public interface IRabbitMQPersistentConnection : IDisposable
     {
+        public bool IsConnected { get; }
+        public bool TryConnect();
+
+        IModel CreateModel();
     }
 }
