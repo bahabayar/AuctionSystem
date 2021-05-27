5434836e2c564e1d612da803606b819e9f367942
Baha <bahabayar@hotmail.com>
2021-05-28 01:21:23 +0300

    Some changes in  Infrastructure layer and seeding processes

--- ESourcing.Order/Startup.cs
+++ ESourcing.Order/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Ordering.Infrastructure;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,6 +27,8 @@ namespace ESourcing.Order
         {
 
             services.AddControllers();
+
+            services.AddInfrastructure(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
